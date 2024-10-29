e64e39b6f955aef31cff6178b169dc0a992bc46c
Bram Wiggers <orangenodes@gmail.com>
2024-10-29 10:55:51 +0100

    Add basic chat functionality

---
+++ chat.py
@@ -0,0 +1,44 @@
+from openai import OpenAI
+import streamlit as st
+
+import numpy as np
+
+st.title("ChatGPT-like clone")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    st.session_state.messages.append({"role": "system", "content": """
+    You are a chatbot assistant named TARS.
+    Always be kind, and put your humor to 60%
+    """})
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        if message["role"] != "system":
+            st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+        # If there is the word "graph" in the response, we will plot a graph
+        if "graph" in response:
+            st.bar_chart(np.random.randn(30, 3))
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
